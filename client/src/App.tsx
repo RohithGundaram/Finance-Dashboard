d69275bb4f318671948caeca8f746d76a97e69c8
Gundaram Raju Rohith <87968757+RohithGundaram@users.noreply.github.com>
2023-07-21 22:49:39 +0530

    intial commmit
    created the basic app and modified the default themes and installed required packages

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import { useMemo } from "react";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
+import { CssBaseline,ThemeProvider } from "@mui/material";
+
+
+function App() {
+  const theme = useMemo(()=> createTheme(themeSettings),[])
+  return (
+    <div className='app'>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        Hello
+      </ThemeProvider>
+    </div>
+  )
+}
+
+export default App
